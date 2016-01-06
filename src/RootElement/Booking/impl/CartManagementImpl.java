3500ec1de71561ec4d5a0ea94eb3523c254b4128
Petroula <gustheodpe@student.gu.se>
2016-01-06 17:02:22 +0100

    added deleteBooking implementation

--- src/RootElement/Booking/impl/CartManagementImpl.java
+++ src/RootElement/Booking/impl/CartManagementImpl.java
@@ -118,12 +118,18 @@ public class CartManagementImpl extends MinimalEObjectImpl.Container implements 
 	/**
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
 	 */
 	public boolean deleteBooking(int bookingID) {
-		// TODO: implement this method
-		// Ensure that you remove @generated or mark it @generated NOT
-		throw new UnsupportedOperationException();
+		if(currentCart == null){
+			return false;
+		}
+		try {	
+			EList<Booking> bookings = currentCart.getBookings();
+			bookings.remove(getBooking(bookingID));
+		} catch (Exception e) {
+			return false;
+		}
+		return true;		
 	}
 
 	/**
